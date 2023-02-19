7496ca900b7b7d6cc3fdb0eb800315187b7c6f16
karmakatoro <karmakatoro@gmail.com>
2023-02-19 13:12:32 +0200

    adding migrations

--- database/migrations/2023_02_19_094808_doctors.php
+++ database/migrations/2023_02_19_094808_doctors.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->string('gender');
             $table->unsignedBigInteger('country_id');
             $table->unsignedBigInteger('role_id');
-            $table->integer('affected');
+            $table->unsignedBigInteger('affected_id');
             $table->integer('phone');
             $table->string('avatar');
             $table->string('bio');
@@ -32,6 +32,7 @@ return new class extends Migration
             $table->timestamps();
             $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('affected_id')->references('id')->on('services')->onDelete('cascade');
         });
     }
 
