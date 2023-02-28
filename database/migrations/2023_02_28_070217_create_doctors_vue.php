34a51cffc937da89d72c297cbe69c18f99d83d41
karmakatoro <karmakatoro@gmail.com>
2023-02-28 09:16:26 +0200

    doctor view created and work well

--- database/migrations/2023_02_28_070217_create_doctors_vue.php
+++ database/migrations/2023_02_28_070217_create_doctors_vue.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('doctors', function (Blueprint $table) {
+        Schema::create('doctors_vue', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('doctors');
+        Schema::dropIfExists('doctors_vue');
     }
 };
